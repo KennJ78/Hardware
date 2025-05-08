3f8e7708c65280c7cb66db3acea490bd9b483129
Gian132004 <giannicolaiduhan@gmail.com>
2025-05-08 12:36:37 +0800

    gg

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
+
+
+
+import React from "react";
 import Login from './pages/Login';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sales from "./pages/Sales";
+import "./Sales.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/sales" element={<Sales />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
 function App() {
   return (
